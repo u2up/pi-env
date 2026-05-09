3b4b1c8baeac76f171c06903f0b0104b369cc328
Samo Pogačnik <samo_pogacnik@t-2.net>
2026-05-09 11:26:21 +0200

    Added initial flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Project with Pi agent runtime";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    common-nix-runtime.url = "git+file:///home/samo/CODEFAB/common-nix-runtime";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, common-nix-runtime, ...}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # Import nixpkgs with rust-overlay
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        commonAgentRuntime = common-nix-runtime.lib.commonAgentRuntime {
+          inherit pkgs;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = commonAgentRuntime ++ [
+            # project-specific tools here
+          ];
+
+          shellHook = ''
+            export PS1="(nix-dev) \u@\h:\w$ "
+            echo "Pi agent runtime loaded"
+          '';
+        };
+      });
+}
